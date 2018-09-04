015777b797fcc11e21e04ac1f1030d8d4d3debca
SolutionsANZ <solutionsanz2@gmail.com>
2018-09-04 15:35:21 +1000

    Adding property file in k8s deployment desriptor

--- dbconfig.js
+++ dbconfig.js
@@ -78,7 +78,7 @@ module.exports = {
   
     // For information on connection strings see:
     // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "sydneyadw_low",
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "critestoltp1_low",
   
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
