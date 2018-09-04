30b0bfa79e0925a9e112acda61a845b2ba360d05
SolutionsANZ <solutionsanz2@gmail.com>
2018-09-04 15:37:01 +1000

    Adding property file in k8s deployment desriptor

--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #microservices..
   #apis4atp..
-  kubectl delete -f deploy/kubernetes/apis4atp-ing.yaml >>/tmp/noise.out
-  kubectl delete -f deploy/kubernetes/apis4atp-svc.yaml >>/tmp/noise.out
-  kubectl delete -f deploy/kubernetes/apis4atp-dpl.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4atp-ing.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4atp-svc.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4atp-dpl.yaml >>/tmp/noise.out
   kubectl delete namespace apis4atp >>/tmp/noise.out
